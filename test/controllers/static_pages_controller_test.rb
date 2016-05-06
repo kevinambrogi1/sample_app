562c596a873bae7f0493d8bab43d972eef841dd1
Kevin Ambrogi <kevinambrogi@hotmail.com>
2016-05-06 18:50:14 +0200

    Eliminate repetition (solves exo 3.1)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,21 +2,24 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
+    @var = "Ruby on Rails Tutorial Sample App"
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{@var}"
   end
 
   test "should get help" do
+    @var = "Ruby on Rails Tutorial Sample App"
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@var}"
   end
 
   test "should get about" do
+    @var = "Ruby on Rails Tutorial Sample App"
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@var}"
   end
 
 end

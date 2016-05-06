8a123153357b0f1c0f4134bb301a1c10040c0d1d
Kevin Ambrogi <kevinambrogi@hotmail.com>
2016-05-06 18:55:11 +0200

    Add a contact page (Solve exo 3.2)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -22,4 +22,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | #{@var}"
   end
 
+  test "should get contact" do
+    @var = "Ruby on Rails Tutorial Sample App"
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@var}"
+  end
+
 end
